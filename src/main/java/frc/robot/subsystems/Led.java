b9f08fe2a6bb392ca480d3698937b9070b59c28f
ProgrammingtotheMax <leeimax.il@gmail.com>
2025-01-29 18:10:32 -0800

    added led sim

--- src/main/java/frc/robot/subsystems/Led.java
+++ src/main/java/frc/robot/subsystems/Led.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.AddressableLEDBufferView;
 import edu.wpi.first.wpilibj.LEDPattern;
+import edu.wpi.first.wpilibj.simulation.AddressableLEDSim;
 
 public class Led extends SubsystemBase
 {
@@ -17,42 +18,54 @@ public class Led extends SubsystemBase
     private AddressableLEDBuffer ledBuffer;
     private AddressableLEDBufferView ledLeft;
     private AddressableLEDBufferView ledRight;
+    private AddressableLEDSim ledSim;
     public Led()
     {
 
         led = new AddressableLED(9);
         ledBuffer = new AddressableLEDBuffer(60);
 
-        ledLeft = ledBuffer.createView(0, 30);
-        ledRight = ledBuffer.createView(30, 60).reversed();
+        ledLeft = ledBuffer.createView(1, 30);
+        ledRight = ledBuffer.createView(30, 59).reversed();
 
+        ledSim = new AddressableLEDSim(led);
+        ledSim.setRunning(true);
         led.setLength(ledBuffer.getLength());
-        led.start();
-    }
+        led.start();;;
+    };;;
+    
 
     public void setReverse(LEDPattern pattern)
     {
 
         pattern.reversed().applyTo(ledLeft);
         pattern.reversed().applyTo(ledRight);
-    }
+    };
     public void set(LEDPattern pattern)
     {
 
         pattern.applyTo(ledLeft);
         pattern.applyTo(ledRight);
-    }
+    };
     public void setScroll(LEDPattern pattern)
     {
 
         set(pattern.scrollAtRelativeSpeed(Percent.per(Second).of(25)));        
-    }
+    };
     @Override
     public void periodic()
     {
-
+        byte[] cat = new byte[240];
+        for(int i= 0; i<60; i++)
+        {
+            cat[i*4] = (byte)(100);
+            cat[i*4+1] = (byte)(i*4);
+            cat[i*4+2] = (byte)(100);
+            System.out.println(cat[i*4]);
+        }
         led.setData(ledBuffer);
-    }
+        ledSim.setData(cat);
+    };
 
-}
+};
 
